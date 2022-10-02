f1c7660320d65ae7bbb889b50904ee570ce9eb57
Samy-2009 <97361048+Samy-2009@users.noreply.github.com>
2022-10-02 14:11:49 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(830,570);
+    canvas.position(110,250);
+}
+
+function draw(){
+    stroke("green");
+    rect(10, 30, 800, 500);
+
+}
